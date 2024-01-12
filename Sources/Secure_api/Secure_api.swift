ddb779cd0e3d280fc87a10a27b8775ef836f4f67
tanvirmhamud <shakilhassan887@gmail.com>
2024-01-12 11:05:56 +0600

    update

--- Sources/Secure_api/Secure_api.swift
+++ Sources/Secure_api/Secure_api.swift
@@ -1,2 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+
+
+@available(macOS 10.15, *)
+class SecureApi : ObservableObject{
+    @Published var loading : String = "Football Secure api";
+
+}
+
